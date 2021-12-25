2c5cd60ada156165155cd8aeb15a5b6d5486c45f
alina-torr <alina.ysic@gmail.com>
2021-12-25 07:37:19 +0300

    Scripted auto-commit on change (2021-12-25 07:37:19) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -31,7 +31,7 @@ public class Server extends AllDirectives {
     private static final String URL_PARAM = "url";
     private static final String COUNT_PARAM = "count";
 
-    private static final String ZOOKEEPER_SERVER = "127.0.0.1:2181";
+    private static final String ZOOKEEPER_SERVER = "127.0.0.1:2182";
     private final ZooKeeper zookeeper;
 
     public Server(ActorSystem system, Http http, Materializer materializer, ActorRef storageActor, int port) throws IOException, InterruptedException, KeeperException {
