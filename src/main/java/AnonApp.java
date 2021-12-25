1386b1a0ee12fabd18f842505446ef311418ef83
alina-torr <alina.ysic@gmail.com>
2021-12-25 05:09:55 +0300

    Scripted auto-commit on change (2021-12-25 05:09:54) by gitwatch.sh

--- src/main/java/AnonApp.java
+++ src/main/java/AnonApp.java
@@ -4,6 +4,6 @@ public class AnonApp {
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
         ActorSystem system = ActorSystem.create("Lab6");
-        
+        ActorRef storageActor = system.actorOf(Props.create(StorageActor.class));
     }
 }
