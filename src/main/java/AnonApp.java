4744a59d05c49008f26ef8ddf854996e33a53d43
alina-torr <alina.ysic@gmail.com>
2021-12-25 05:17:10 +0300

    Scripted auto-commit on change (2021-12-25 05:17:10) by gitwatch.sh

--- src/main/java/AnonApp.java
+++ src/main/java/AnonApp.java
@@ -20,7 +20,7 @@ public class AnonApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-
+        
 
 
         final Flow<HttpRequest, HttpResponse, NotUsed> flow = null;
