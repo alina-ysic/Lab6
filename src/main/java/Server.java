d6910bbee946a60a9da4444966b1969c94e905a3
alina-torr <alina.ysic@gmail.com>
2021-12-25 05:42:11 +0300

    Scripted auto-commit on change (2021-12-25 05:42:11) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,7 @@
 import akka.http.javadsl.server.AllDirectives;
 
 public class Server extends AllDirectives {
-    
+    private final Http http;
+    private final Materializer materializer;
+    private final Flow<HttpRequest, HttpResponse, NotUsed> routes;
 }
