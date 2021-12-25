4b444b8e59c6e4c1988fec63938df9a962222cd6
alina-torr <alina.ysic@gmail.com>
2021-12-25 05:12:43 +0300

    Scripted auto-commit on change (2021-12-25 05:12:43) by gitwatch.sh

--- src/main/java/AnonApp.java
+++ src/main/java/AnonApp.java
@@ -1,8 +1,16 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class AnonApp {
     public static void main(String[] args) {
@@ -12,14 +20,18 @@ public class AnonApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
+        
+
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow = null;
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 flow,
-                ConnectHttp.toHost(HOST, port),
+                ConnectHttp.toHost("localhost", port),
                 materializer
         );
 
-        System.out.println("Server started: " + HOST + ":" + port);
+        System.out.println("Server started: " + "localhost" + ":" + port);
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
