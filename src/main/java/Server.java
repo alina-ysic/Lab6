68dc43858299cfd9102292dad93ddb36a1e5e45b
alina-torr <alina.ysic@gmail.com>
2021-12-25 08:55:33 +0300

    Scripted auto-commit on change (2021-12-25 08:55:33) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -57,7 +57,6 @@ public class Server extends AllDirectives {
         return get(() ->
                 parameter(URL_PARAM, url ->
                     parameter(COUNT_PARAM, countValue -> {
-                        System.out.println("request");
                         int count = Integer.parseInt(countValue);
                         if (count == 0) return completeWithFuture(http.singleRequest(HttpRequest.create(url)));
                         return completeWithFuture(Patterns.ask(storageActor, new RandomRequest(), TIMEOUT)
