ecac67d0588b17dcf710b2c517781774aa1e63d5
alina-torr <alina.ysic@gmail.com>
2021-12-25 05:43:11 +0300

    Scripted auto-commit on change (2021-12-25 05:43:11) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,9 +4,16 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
 
 public class Server extends AllDirectives {
     private final Http http;
     private final Materializer materializer;
     private final Flow<HttpRequest, HttpResponse, NotUsed> routes;
+
+    public Server(Http http, Materializer materializer, Flow<HttpRequest, HttpResponse, NotUsed> routes) {
+        this.http = http;
+        this.materializer = materializer;
+        this.routes = routes;
+    }
 }
