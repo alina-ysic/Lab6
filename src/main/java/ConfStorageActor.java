cf697961231b2b0df082637991b69a0acc375b93
alina-torr <alina.ysic@gmail.com>
2021-12-25 06:41:57 +0300

    Scripted auto-commit on change (2021-12-25 06:41:57) by gitwatch.sh

--- src/main/java/ConfStorageActor.java
+++ src/main/java/ConfStorageActor.java
@@ -1,8 +1,12 @@
 import akka.actor.AbstractActor;
 
 public class ConfStorageActor extends AbstractActor {
+
+    private String[] servers;
+
     @Override
     public Receive createReceive() {
+        
         return null;
     }
 }
