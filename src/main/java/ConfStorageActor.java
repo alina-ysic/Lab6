b83a6f2c38b7bd1555d3056d872267ede008d1ab
alina-torr <alina.ysic@gmail.com>
2021-12-25 06:42:32 +0300

    Scripted auto-commit on change (2021-12-25 06:42:32) by gitwatch.sh

--- src/main/java/ConfStorageActor.java
+++ src/main/java/ConfStorageActor.java
@@ -4,9 +4,13 @@ public class ConfStorageActor extends AbstractActor {
 
     private String[] servers;
 
+    public String getServer() {
+        return 
+    }
+
     @Override
     public Receive createReceive() {
-        
+
         return null;
     }
 }
