b45b2aa82bb33fa0575e03fe83d05b1b9a8a29dd
alina-torr <alina.ysic@gmail.com>
2021-12-25 05:11:09 +0300

    Scripted auto-commit on change (2021-12-25 05:11:09) by gitwatch.sh

--- src/main/java/AnonApp.java
+++ src/main/java/AnonApp.java
@@ -1,11 +1,15 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class AnonApp {
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
         ActorSystem system = ActorSystem.create("Lab6");
         ActorRef storageActor = system.actorOf(Props.create(ConfStorageActor.class));
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
