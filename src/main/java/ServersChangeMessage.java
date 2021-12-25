bbc46e1500f09f627b8836e746529c24c08a5b49
alina-torr <alina.ysic@gmail.com>
2021-12-25 07:02:58 +0300

    Scripted auto-commit on change (2021-12-25 07:02:58) by gitwatch.sh

--- src/main/java/ServersChangeMessage.java
+++ src/main/java/ServersChangeMessage.java
@@ -1,2 +1,12 @@
 public class ServersChangeMessage {
+
+    private String[] servers;
+
+    public ServersChangeMessage(String[] servers) {
+        this.servers = servers;
+    }
+
+    public String[] getServers() {
+        return servers;
+    }
 }
