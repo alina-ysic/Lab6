2d034f02c60da64e03e950484639ff19ff2c81ca
alina-torr <alina.ysic@gmail.com>
2021-12-25 07:48:05 +0300

    Scripted auto-commit on change (2021-12-25 07:48:05) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -40,7 +40,7 @@ public class Server extends AllDirectives {
         this.storageActor = storageActor;
         this.zookeeper = new ZooKeeper(ZOOKEEPER_SERVER, TIMEOUT_INT, null);
         zookeeper.create(
-                NODE_PATH,
+                SERVERS_PATH,
                 ("http://localhost" + port).getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL_SEQUENTIAL
